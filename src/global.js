3f351c6a535faf8e4223d3752a9f0bd4e3e16866
ninko-txz <yamamoto.tar.gz@gmail.com>
2025-11-04 03:40:50 +0900

    [Fix] v1.1.4 Avoid access count from dev environment

--- src/global.js
+++ src/global.js
@@ -1,5 +1,11 @@
 'use strict';
 
+if (location.origin == 'https://ninko.neocities.org') {
+    const path = location.pathname.replaceAll('/', '');
+    const page = path == '' ? 'index' : path;
+    import(`https://ninko.pythonanywhere.com/${page}/neostat.js`);
+}
+
 document.querySelectorAll('img').forEach((img) => {
     img.addEventListener('contextmenu', (ev) => ev.preventDefault());
 });
